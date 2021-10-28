e4bd2c1e5e995fbcb9887b3181e242fa34954a80
hieuchkxi <90381434+hieuchkxi@users.noreply.github.com>
2021-10-29 01:55:58 +0700

    kết nối cơ sở dữ liệu

--- src/quanly.php
+++ src/quanly.php
@@ -11,20 +11,30 @@
 
     <title>Quản lý</title>
 </head>
+
+<div class="container">
 <table class="table">
   <thead>
     <tr>
       <th scope="col">Họ tên</th>
       <th scope="col">Tuổi</th>
       <th scope="col">Địa chỉ</th>
-      <th scope="col">email</th>
+      <th scope="col">Email</th>
+      <th scope="col">Kế hoạch</th>
+      <th scope="col">Sửa</th>
+      <th scope="col">Xóa</th>
     </tr>
 </table>
-<div class="container">
-<button type="button" style="background-color:yellow;margin-left:auto;margin-right:auto;display:block;margin-top:22%;margin-bottom:0%">
-   mybuttonname
-</button> 
+<tbody>
+    <tr>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    </tbody>
 </div>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
         </script>
