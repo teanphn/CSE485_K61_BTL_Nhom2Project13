33a39b732a0e44af07252eed1ec3dc7d780977a3
bnmbnmmnb <quanghong29915@gmail.com>
2021-11-04 07:31:55 +0700

    fix signup

--- src/process_signup.php
+++ src/process_signup.php
@@ -8,22 +8,22 @@ if(isset($_POST['btsignup'])){
     $usernmsu = $_POST['txtusersu'];   
     $pass2 = $_POST['txtPass2'];
     $pass3 = $_POST['txtPass3'];
+    $check=true;
     if ($pass2 != $pass3){
         $_SESSION['noti'] = '<p class="text-danger">Mật Khẩu Chưa Trùng Khớp</p>'; 
-        header("location:signup.php");
+        $check=false;
     }
     if ($namesu == ''or $agesu == '' or $andr == '' or $mailsu == '' or $usernmsu == '' or $pass2 == ''  ){ 
         $_SESSION['noti'] = '<p class="text-danger">Vui lòng điền đầy đủ thông tin</p>'; 
-        header("location:signup.php");
-
+        $check=false;
     }
 
-
+ if ($check==true){
     $helper = $usernmsu ;
     $sql = "INSERT INTO `user` ( `ho ten`, `dia chi`, `tuoi`, `email`, `loai`, `taikhoan`, `matkhau`) 
     VALUES ( '$namesu','$andr','$agesu','$mailsu','2','$usernmsu','$pass2')";
 
-   if (mysqli_query($conn, $sql)){
+   if (mysqli_query($conn, $sql) === true){
                    //fix thiếu idus
                     $sql_1 = "SELECT * FROM `user` Where `taikhoan` = '$helper'";
                     $result = mysqli_query($conn, $sql_1);
@@ -32,13 +32,18 @@ if(isset($_POST['btsignup'])){
                     $idus = $row['iduser'];
                     $sql_2 ="INSERT INTO `kehoach` ( `idus`) VALUES ( '$idus') " ;
                     $result_2 = mysqli_query($conn, $sql_2);
+                    header("location:index.php");
                     }
                    //--
-    header("location:index.php");
+
     }
     else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
    }
+         }
+    else{
+        header("location:signup.php");
+    }
 
 
 }
